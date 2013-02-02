37a4a03d1037dc28be1262fa3d422c0e411e7cfd
Christopher W. Johnson <christopherwjohnson@gmail.com>
2013-02-02 12:35:34 -0700

    Lots of little details.  Got unit testing setup.  Starting to test populating and connecting quest data.

---
+++ test/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+chdir(dirname(__FILE__) . '/../../..');
+//print getcwd();
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+require_once './includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+require_once './includes/bootstrap.inc';
+module_load_include('inc', 'ground_php', 'test/Ground_Test_Fixtures');
+module_load_include('inc', 'ground_drupal', 'test/Drupal_Test_Fixtures');
+require_once 'sites/all/test/Jester_Test_Fixtures.inc';
---
+++ test/tests/Quest_Test.php
@@ -0,0 +1,26 @@
+<?php
+
+class Quest_Test extends Jester_Test_Fixtures {
+
+  function setUp() {
+    $this->fixture_load_ground();
+    $this->prepare_database();
+    $this->populate_database();
+  }
+
+  function test_quest_types() {
+    $objects = $this->ground->create_query('quest_type')->run();
+    $this->assertTrue(count($objects) > 0);
+    $this->assertEquals('kingdom', $objects[0]->name);
+  }
+
+  function test_quests() {
+    $objects = $this->ground->create_query('quest')->run();
+    print_r($objects);
+    $this->assertTrue(count($objects) > 0);
+    $this->assertEquals('MarlothDB', $objects[0]->name);
+    $this->assertInstanceOf('stdClass', $objects[0]->quest_type);
+    $this->assertEquals('kingdom', $objects[0]->quest_type->name);
+  }
+
+}
